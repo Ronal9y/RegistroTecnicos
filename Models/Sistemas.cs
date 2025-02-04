67900fca428bc61395f6da8f54cc23f6b93c8f93
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-04 16:33:12 -0400

    Creando la tabla de sistemas

---
+++ Models/Sistemas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+    public class Sistemas
+    {
+
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Descripcion { get; set; }
+        [Range(1, 100, ErrorMessage = "La complejidad deba estar entre 1 y 100")]
+        public double Complejidad { get; set; }
+        
+    }
+}
